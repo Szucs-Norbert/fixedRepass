1761696b64ea8595d335ef8707d15d83624a6359
Norbi <szucs.norb3rt0424@gmail.com>
2021-09-06 12:20:44 +0200

    javítaás alatt

--- src/App.java
+++ src/App.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        aboutShow();
-        //Fejrész kiírása
-        System.out.println("Jelszavak");
-        //Verzió kiírása
-        System.out.println("Verzió: 0.0.1");
+        int success=0;
         
-     
+        aboutShow();
+        bekeres();
+       
+        
+       
+        
+
+    public static bekeres(){
         Scanner scan = new Scanner(System.in);        
 
         System.out.print("Felhasználónév: ");    
@@ -21,13 +24,14 @@ public class App {
         System.out.print("Hely: ");
         String place = scan.nextLine();
         scan.close();
-        int success = 0;
+
+        return 
+
+    }
+
+    public static void ellenorzes(){
         try {
-            /* 
-            A jelszó, a felhasználónév és a 
-            használati helye a profile 
-            objektumban van tárolva            
-            */
+            
             Profile profile = new Profile(user , password, place);
             FileWriter writeFile = new FileWriter("password.txt");
             PrintWriter writePrint = new PrintWriter(writeFile);
@@ -36,6 +40,7 @@ public class App {
             writePrint.print(profile.place);
             writePrint.close();
             success = 1;
+
         } catch (IOException exception) {
             System.err.println("Hiba! A fájlbaírás sikertelen. Keresse meg a fejlesztőt.");
         }
@@ -43,10 +48,13 @@ public class App {
         if(success == 1) {
              System.out.println("Ok. A kiírás sikeres.");  }
         else {  System.out.println("Hiba! A kiírás sikertelen"); }
-
+   
     }
 
     public static void aboutShow() {
         System.out.println("Nagy János");
+        System.out.println("Jelszavak");     
+        System.out.println("Verzió: 0.0.1");
     }
+    
 }
